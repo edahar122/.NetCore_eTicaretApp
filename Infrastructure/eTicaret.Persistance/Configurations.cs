cadba39752462437eabce436a6a51ec8581cca9e
Serhat Har <serhatrah83@gmail.com>
2025-02-19 15:25:39 +0300

    migrations

---
+++ Infrastructure/eTicaret.Persistance/Configurations.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTicaret.Persistance {
+    static class Configurations {
+        public static string ConnectionString {
+            get {
+                ConfigurationManager configurationManager = new();
+                configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "C:\\proje\\eTicaretApp\\Presentation\\eTicaret.API"));
+                configurationManager.AddJsonFile("appsettings.json");
+                return configurationManager.GetConnectionString("PostgreSQL");
+            }
+        }
+    }
+}
--- Infrastructure/eTicaret.Persistance/ServiceRegistration.cs
+++ Infrastructure/eTicaret.Persistance/ServiceRegistration.cs
@@ -1,13 +1,12 @@
 ﻿using ETicaret.Persistance.Contexts;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace eTicaret.Persistance {
     public static class ServiceRegistration {
         public static void AddPersistanceServices(this IServiceCollection services) {
-            //string connectionString = "User ID=postgres;Password=123;Host=localhost;Port=5432;Database=postgres;\r\n";
-
-            services.AddDbContext<ETicaretDBContext>(options => options.UseNpgsql("User ID=postgres;Password=123;Host=localhost;Port=5432;Database=eTicaret;\r\n"));
+            services.AddDbContext<ETicaretDBContext>(options => options.UseNpgsql(Configurations.ConnectionString));
         }
     }
 }
